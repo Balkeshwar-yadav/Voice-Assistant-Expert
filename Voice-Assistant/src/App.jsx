723446f77c1a2568e2457f47a5624bec613e459e
Balkeshwar-yadav <balkeshwar42@gmail.com>
2026-01-04 15:57:18 +0530

    Add files via upload

---
+++ Voice-Assistant/src/App.jsx
@@ -0,0 +1,204 @@
+import React, { useState,useEffect } from 'react'
+import img from './ai-human.jpeg'
+const App = () => {
+
+  const[transcript,setTranscript] = useState("")
+  const[isListening,setIsListening] = useState(false)
+  const[information,setInformation] = useState("")
+  const[voices,setVoice] = useState([])
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+  const loadVoice = () =>{
+    const allVoice = window.speechSynthesis.getVoices();
+    setVoice(allVoice)
+  }
+  const undefined = ()=>{
+    console.log("....");
+  }
+
+  useEffect(()=>{
+    if(window.speechSynthesis.onvoiceschanged !== undefined){
+      window.speechSynthesis.onvoiceschanged = loadVoice
+    }else{
+      loadVoice();
+    }
+  },[])
+
+  const startListening = () =>{
+    recognition.start();
+    setIsListening(true);
+  }
+
+
+recognition.onresult = (event) =>{
+  const spokenText = event.results[0][0].transcript.toLowerCase();
+  setTranscript(spokenText);
+  handleVoiceCommand(spokenText)
+}
+
+recognition.onend = ()=>setIsListening(false)
+
+const speakText = (text)=>{
+   if(voices.length === 0){
+    console.warn("No voice available yet..")
+    return;
+   }
+
+   const utterance = new SpeechSynthesisUtterance(text);
+
+   const maleEnglishVoice = voices.find((voice)=>
+    voice.lang.startsWith("en-") && voice.name.toLowerCase().includes("male")) || voices.find((voice)=>voice.lang.
+  startsWith("en-")) || voices[0]
+
+
+   utterance.voice = maleEnglishVoice;
+   utterance.lang = maleEnglishVoice.lang || "en-US";
+   utterance.rate = 1;
+   utterance.pitch = 1;
+   utterance.volume= 1;
+
+   window.speechSynthesis.speak(utterance);
+
+}
+
+
+const handleVoiceCommand = async(command)=>{
+  if(command.startsWith("open ")){
+    const site = command.split("open ")[1].trim();
+
+    const sitesMap = {
+       youtube: "https://www.youtube.com",
+        facebook: "https://www.facebook.com",
+        google: "https://www.google.com",
+        twitter: "https://www.twitter.com",
+        instagram: "https://www.instagram.com",
+    };
+    if (sitesMap[site]) {
+        speakText(`Opening ${site}`);
+        window.open(sitesMap[site], "_blank");
+        setInformation(`Opened ${site}`);
+      } else {
+        speakText(`I don't know how to open ${site}`);
+        setInformation(`Could not find the website for  ${site}`);
+      }
+      return;
+  }
+
+  if(command.includes("what is your name")){
+    const response = "Hello sir I'm AI Expert, Your voice assistant created by Balkeshwar";
+    speakText(response);
+    setInformation(response);
+    return;
+  }else if(command.includes("Hello AI")){
+    const response = "Hello sir I'm AI Expert, How can i help you";
+    speakText(response);
+    setInformation(response);
+    return;
+  }else if(command.includes("what is your age")){
+    const response = "Hello sir I'm AI Expert, I'm 1 month old";
+    speakText(response);
+    setInformation(response);
+    return;
+  }else if(command.includes("Who are you")){
+    const response = "Hello sir I'm  your AI Expert voice assistant";
+    speakText(response);
+    setInformation(response);
+    return;
+  }
+
+
+    // List of famous people
+    const famousPeople = [
+      "bill gates",
+      "mark zuckerberg",
+      "elon musk",
+      "steve jobs",
+      "warren buffet",
+      "barack obama",
+      "jeff bezos",
+      "sundar pichai",
+      "mukesh ambani",
+      "virat kohli",
+      "sachin tendulkar",
+      "brian lara",
+    ];
+
+    if(famousPeople.some((person)=>command.includes(person))){
+      const person = famousPeople.find((person)=>command.includes(person))
+      const personData = await fetchPersonData(person)
+
+      if(personData){
+        const infoText = `${personData.name}, ${personData.extract}`
+        setInformation(infoText)
+        speakText(infoText)
+
+        performGoogleSeach(command)
+      }else{
+        const fallbackMessage = "I couldn't find detailed information"
+      
+        speakText(fallbackMessage)
+        performGoogleSeach(command)
+      }
+
+    }else{
+      const fallbackMessage = `Here is the information about ${command}`;
+
+      speakText(fallbackMessage);
+      performGoogleSeach(command);
+    }
+  }
+
+  const fetchPersonData = async (person) =>{
+    const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
+      person
+    )}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if(data && data.title && data.extract){
+        return {
+          name:data.title,
+          extract:data.extract.split('.')[0]
+        }
+      }else{
+        return null
+      }
+      
+    } catch (error) {
+      console.error('error')
+      return null
+    }
+
+  }
+
+  const performGoogleSeach = (query) =>{
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+
+    window.open(searchUrl,"_blank");
+   
+}
+
+
+
+
+  return (
+    <div>
+      <div className="voice-assistant">
+        <img src={img} alt="AI" className='ai-image' />
+        <h2>Voice Assistant expert</h2>
+        <button className='btn' onClick={startListening} disabled={isListening} >
+          <i className='fas fa-microphone '></i>
+          {isListening ? " Listening... " :" Start Listening "}
+       
+        </button>
+         <p className='transcript'>{transcript}</p>
+          <p className="information">{information}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
